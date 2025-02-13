aa7619a17139c398aa34cc2219933cbf4dcb53a3
Trenton Hoffman <trenton.hoffman@gmail.com>
2025-02-12 22:24:34 -0500

    FastAPI endpoints for sms/mms webhooks

--- src/message_api.py
+++ src/message_api.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI, HTTPException
 
+from src.message_models import InboundMmsMessage, InboundSmsMessage
+
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello World"}
+@app.post("/webhook/sms")
+async def receive_sms(message: InboundSmsMessage):
+    try:
+        print(f"Received SMS from {message.from_address} to {message.to_address} at {message.timestamp}")
+        return {"response": "SMS Received"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
-@app.post("/")
-async def put_root(payload: dict):
-    return {"body": "Processed payload {}".format(payload)}
+@app.post("/webhook/mms")
+async def receive_mms(message: InboundMmsMessage):
+    try:
+        print(f"Received MMS from {message.from_address} to {message.to_address} at {message.timestamp}")
+        return {"response": "MMS Received"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
--- test/test_message_api.py
+++ test/test_message_api.py
@@ -1,22 +1,53 @@
 from fastapi.testclient import TestClient
 
+from src.message_models import InboundMmsMessage, InboundSmsMessage
 from src.message_api import app
 
 client = TestClient(app)
 
 
-def test_read_root():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
-
-
-def test_put_root():
+def test_receive_sms():
     payload = {
-        "message": "Goodnight Moon"
+        "from_address": "1234567890",
+        "to_address": "0987654321",
+        "body": "On my way with donuts!",
+        "timestamp": "2021-01-01T00:00:00Z",
+        "xillio_id": "1234"
     }
-
-    response = client.post("/", json=payload)
+    response = client.post("/webhook/sms", json=payload)
+    print(response)
     assert response.status_code == 200
-    assert response.json() == {"body": "Processed payload {'message': 'Goodnight Moon'}"}
+    assert response.json() == {"response": "SMS Received"}
+
+def test_receive_mms():
+    payload = {
+        "from_address": "1234567890",
+        "to_address": "0987654321",
+        "body": "On my way with donuts!",
+        "timestamp": "2021-01-01T00:00:00Z",
+        "xillio_id": "1234"
+    }
+    response = client.post("/webhook/mms", json=payload)
+    assert response.status_code == 200
+    assert response.json() == {"response": "MMS Received"}
+
+def test_receive_sms_invalid():
+    payload = {
+        "from_address": "1234567890",
+        "to_address": "0987654321",
+        "body": "On my way with donuts!",
+        "timestamp": "2021-01-01T00:00:00Z"
+    }
+    response = client.post("/webhook/sms", json=payload)
+    assert 400 <= response.status_code < 500
+
+def test_receive_mms_invalid():
+    payload = {
+        "from_address": "1234567890",
+        "to_address": "0987654321",
+        "body": "On my way with donuts!",
+        "timestamp": "2021-01-01T00:00:00Z"
+    }
+    response = client.post("/webhook/mms", json=payload)
+    assert 400 <= response.status_code < 500
 
